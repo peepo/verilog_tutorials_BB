3fdcc819b6ef869015cba6a8d6c528ac1a1738f2
Jonathan Chetwynd <jay@peepo.com>
2016-01-11 15:59:36 +0000

    PWM3 files

---
+++ project_5/PWM_top.v
@@ -0,0 +1,19 @@
+module mojo_top(
+    // 12MHz clock input
+    input clk,
+    // Input from buttons (active low)
+//    input [3:0]sw_n,
+    // Outputs to the 8 onboard LEDs
+    output[7:0]LED,
+    );
+
+wire rst = 1'b0;//~sw_n[3]; // make reset active high
+
+led_wave #(.CTR_LEN(25)) wave(
+    .rst(rst),
+    .clk(clk),
+    .led(LED)
+  );  
+
+
+endmodule
